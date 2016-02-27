9920cbcbc524facc264f428342fbd5facf5ae5fb
Gouthamarajan NT <goutham.thesoph@gmail.com>
2016-02-27 18:25:03 +0530

    first

---
+++ hello.js
@@ -0,0 +1,7 @@
+
+function myFunction() {
+    var display = document.querySelector('#display');
+    var p = document.createElement('p');
+    p.textContent='Hello World';
+    display.appendChild(p);
+}
